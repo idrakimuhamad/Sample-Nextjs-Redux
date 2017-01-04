27d50e5eb926f41c569fe9524fcb1146e603dde3
Idraki Muhamad <idrakimuhamad@gmail.com>
2017-01-04 15:12:48 +0800

    Added tachyons

--- components/Head.js
+++ components/Head.js
@@ -4,5 +4,11 @@ export default () => (
     <title>Something</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link rel="stylesheet" href="static/css/app.css"/>
+    <style>{
+      `.container {
+        max-width: 1140px;
+      }`
+    }
+    </style>
   </Head>
 )
--- pages/index.js
+++ pages/index.js
@@ -29,13 +29,25 @@ export default class Counter extends React.Component {
 
     render () {
         return (
-            <div>
+            <div className="app">
                 <Head />
                 <Provider store={this.store}>
-                    <Clock />
+                    <div className="index-page">
+                      <header className="index-page-header">
+                        <nav className="w-100 center container">
+                          <div className="pa3 pa4-ns">
+                            <a className="link w2 dim black b f6 f5-ns dib mr3 grow-large border-box logo" href="#" title="Home">
+                              <svg className="link white-90 hover-white" data-icon="skull" viewBox="0 0 32 32"><title>skull icon</title><path d="M16 0 C6 0 2 4 2 14 L2 22 L6 24 L6 30 L26 30 L26 24 L30 22 L30 14 C30 4 26 0 16 0 M9 12 A4.5 4.5 0 0 1 9 21 A4.5 4.5 0 0 1 9 12 M23 12 A4.5 4.5 0 0 1 23 21 A4.5 4.5 0 0 1 23 12"></path></svg>
+                            </a>
+                            <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Home</a>
+                            <a className="link dim gray f6 f5-ns dib mr3" href="#" title="About">About</a>
+                            <a className="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Store</a>
+                            <a className="link dim gray f6 f5-ns dib" href="#" title="Contact">Contact</a>
+                          </div>
+                        </nav>
+                      </header>
+                    </div>
                 </Provider>
-
-                <Link href='/about'><a className="measure lh-copy f6 db no-underline bg-animate bg-white hover-bg-light-blue black">About</a></Link>
             </div>
         )
     }
